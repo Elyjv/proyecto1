4511cb1250f25c50e1ffafeb937bacf97d129282
Ely <elyverdugo88@gmail.com>
2024-05-14 16:32:27 -0300

    cambios en el menu

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
 const sidebar = document.getElementById('sidebar');
 const btn = document.getElementById('trigger-btn');
-const menuTex = document.getElementByClassName('menu-tex');
+const menuText = document.getElementsByClassName('menu-tex');
 
 let sidebarActive = true;
 
 btn.addEventListener('click', () => {
     if (sidebarActive) {
-        sidebar.style.opacity = '100px';
+        sidebar.style.width = '0'; // Ajusta el ancho a cero para ocultar el menú
         sidebarActive = false;
         for (let i = 0; i < menuText.length; i++) {
             menuText[i].style.display = 'none';
-
         }
-    }
-    else {
-        sidebar.style.width = '250px';
+    } else {
+        sidebar.style.width = '250px'; // Restaura el ancho a 250px para mostrar el menú
         sidebarActive = true;
         for (let i = 0; i < menuText.length; i++) {
             menuText[i].style.display = 'flex';
         }
-
     }
 });
+
